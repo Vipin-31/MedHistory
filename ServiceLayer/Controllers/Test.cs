a799a111bc5b3f1315f38f7e0b103c6e01f02ab2
Anuro0p <mews5484@gmail.com>
2022-04-24 12:41:03 +0530

    initial push

---
+++ ServiceLayer/Controllers/Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class Test : Controller
+    {
+        [HttpGet]
+        public JsonResult test()
+        {
+            return Json(1);
+        }
+    }
+}
